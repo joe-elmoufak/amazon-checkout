c9d4aaea38ee8344134dc1d7c54fe735e5cb5b6d
joe-elmoufak <Joe.elmoufak@so.energy>
2023-04-23 18:18:27 +0100

    Added timeouts to configfile

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://www.amazon.co.uk'
+    baseUrl: 'https://www.amazon.co.uk',
+    defaultCommandTimeout: 30000,
+    pageLoadTimeout: 60000
   },
 });
