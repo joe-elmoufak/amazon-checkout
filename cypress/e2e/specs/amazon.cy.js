acc349f68c777fadfbf7a98631a77bd1b238853f
joe-elmoufak <Joe.elmoufak@so.energy>
2023-04-23 18:13:55 +0100

    Refactor

--- cypress/e2e/specs/amazon.cy.js
+++ cypress/e2e/specs/amazon.cy.js
@@ -2,7 +2,10 @@ import {addProductsToBasket, removeItemFromBasket} from "../pages/amazon";
 
 describe('Amazon - Add to basket functionality', () => {
     it('Add multiple items types to the shopping basket, then modify the basket', function() {
-        addProductsToBasket(['vacuum', 'cooker', 'blenders'], 3)
-        removeItemFromBasket(9)
+        const itemsToAdd = 3
+        const productCategories = ['vacuum', 'cooker', 'blenders']
+
+        addProductsToBasket(productCategories, itemsToAdd)
+        removeItemFromBasket(itemsToAdd * productCategories.length)
     })
 })
